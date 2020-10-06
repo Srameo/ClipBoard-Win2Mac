fd850c23625cf467362406d8d73319976bc1edc2
srameo <870830732@qq.com>
2020-10-06 08:38:54 +0800

    server 发回数据

--- src/utils/net/core/server.py
+++ src/utils/net/core/server.py
@@ -12,6 +12,7 @@ def service(sock, addr):
         time.sleep(0.5)  # 延时0.5秒，没接收到数据或者exit则退出
         if not message or message.decode("utf-8") == "sr@meo":
             break
+        sock.send(b"received!")
         pyperclip.copy(message.decode("utf-8"))
     sock.send(b"sr@meo")
     sock.close()
